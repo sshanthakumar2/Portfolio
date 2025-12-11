8c4e171502f945b96a484c0cc4731107285768a7
sshanthakumar2 <sshanthakumar2@myseneca.ca>
2025-12-11 12:53:15 -0500

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ export const Hero = () => {
     return <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hi I'm Sampavi</h1>
-            <p className={styles.description}>I'm a frontend developer with 3 years of 
-               experience using React and NodeJS.</p>
+            <p className={styles.description}>I'm a full-stack developer with over 3 years of experience building
+                    applications using JavaScript, React, and Node.js.</p>
             <a className={styles.contactBtn} href="mailto:sampavi.shanthakumar@gmail.com">Contact Me</a>
         </div>
         <img className={styles.HeroImg} src={getImageUrl("hero/myImage.jpeg")} alt="Hero Image of me" />
